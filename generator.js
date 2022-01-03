320d196b2ddbffb8140874a447e1b43238b1f2fb
Dylan Littlewood <dylanlittlewood@live.co.uk>
2022-01-03 22:35:54 +0000

    Added custom testing to the script.

--- generator.js
+++ generator.js
@@ -1,3 +1,15 @@
 
+const test = func => {
+    if(func()){
+        return 'Passed';
+    } else {
+        return 'Failed'
+    }
+}
+
+const testOne = () => {
+    return true;
+}
+
 console.log('Starting Tests...');
-console.log('  Test 1: ');
+console.log('  Test 1:', test(testOne));
